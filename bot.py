5b99b39b795740934839f9ac601e242bda800f6a
Alex The JPEG <alexthejpeg@fastmail.com>
2024-07-09 21:24:49 -0400

    Roll numbers according to cron

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+import aiocron
 import discord
+import random
 import tomllib
 
 from pathlib import Path
@@ -12,6 +14,19 @@ with Path("bot_settings.toml").open("rb") as file:
 @bot.event
 async def on_ready() -> None:
     print("Bot is ready!")
+
+
+@aiocron.crontab(settings["bot"]["cron"])
+async def roll():
+    channel = bot.get_channel(settings["bot"]["channel"])
+    users = settings["bot"]["users"]
+    
+    generated_numbers = [random.randint(1, 1000) for _ in users]
+    smallest_number = min(generated_numbers)
+    largest_number = max(generated_numbers)
+
+    for index in range(len(users)):
+        await channel.send(f"<@{users[index]}> {generated_numbers[index]}")
 
 
 bot.run(settings["bot"]["token"])
